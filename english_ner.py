816824fc6f6a7c17d7d315183833537c858218d1
BillelBenoudjit <hb_benoudjit@esi.dz>
2022-03-07 15:27:12 +0100

    Refactor entities prediction for english models

--- english_ner.py
+++ english_ner.py
@@ -16,12 +16,7 @@ class Annotation:
         self.tag = tag
 
 
-id2label = {0: 'O', 1: 'B-disease', 2: 'I-disease'}
-
-model_id = "data/clinical_ner_bluebert_pubmed_mimic_uncased"
-
-
-async def predict_ner_bluebert(sentence):
+async def predict_english_ner(sentence, model_id, id2label):
     sentence = sentence.lower()
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     inputs = tokenizer(sentence, return_offsets_mapping=True, return_tensors="pt")
@@ -38,6 +33,22 @@ async def predict_ner_bluebert(sentence):
     predictions = [id2label[id] for id in flat_pred if id != -100]
 
     tags = predictions[1:len(predictions) - 1]
+
+    cpt = 0
+    temp = "O"
+    for code, tag in zip(encode, tags):
+        if code.startswith("##") and tag.startswith("O") and not temp.startswith("O"):
+            tags[cpt] = "I" + temp[1:]
+
+        if code.startswith("##") and tag.startswith("B"):
+            tags[cpt] = tag.replace("B", "I")
+        '''
+        if tags[cpt] != 'O':
+            print("code: ", code, "tag before: ", tag, "tag after: ", tags[cpt])
+        '''
+        temp = tags[cpt]
+        cpt += 1
+
     pos_tags = [pos for token, pos in pos_tag(encode)]
     conlltags = [(token, pos, tg) for token, pos, tg in zip(encode, pos_tags, tags)]
     ne_tree = conlltags2tree(conlltags)
@@ -45,22 +56,43 @@ async def predict_ner_bluebert(sentence):
     for subtree in ne_tree:
         # skipping 'O' tags
         if type(subtree) == Tree:
+            # print('subtree:', subtree)
+            # print('subtree leaves:', subtree.leaves())
             original_label = subtree.label()
             original_text = " ".join([token for token, pos in subtree.leaves()])
             start_words = [word for word in original_text.split(" ") if "#" not in word]
+            # print("start_words: ", start_words)
             entities = []
-            if len(start_words) <= 2:
-                if len(start_words) == 2:
-                    start = original_text.index(start_words[0])
-                    end = original_text.index(start_words[1])
-                    entities.append(original_text[start:end].replace(" ##", ""))
-                    entities.append(original_text[end:].replace(" ##", ""))
+            if not original_text.startswith("##"):
+                if len(start_words) <= 2:
+                    if len(start_words) == 2:
+                        start = original_text.index(start_words[0])
+                        end = original_text.index(start_words[1])
+                        # print(original_text[start:end])
+                        # print(original_text[end:])
+                        entities.append(original_text[start:end].replace(" ##", ""))
+                        entities.append(original_text[end:].replace(" ##", ""))
+                    else:
+                        # print("One word in the original_text: ", original_text)
+                        entities.append(original_text.replace(" ##", ""))
+                        # entities = [entity for entity in original_text.split("##") if len(entity) != 0]
                 else:
-                    entities = [entity for entity in original_text.split("##") if len(entity) != 0]
+                    for i in range(len(start_words)):
+                        if i < len(start_words) - 1:
+                            start = original_text.index(start_words[i])
+                            end = original_text.index(start_words[i + 1])
+                            entities.append(original_text[start:end].replace(" ##", ""))
+                        else:
+                            temp = original_text.index(start_words[i])
+                            entities.append(original_text[temp:].replace(" ##", ""))
                 original_text = "".join([entity for entity in entities])
                 annotated_text.append(
                     Annotation(original_text, original_label)
                 )
+    '''
+    for entity in annotated_text:
+        print(entity.entity, entity.tag)
+    '''
 
     finalTokens = []
     tokens = []
--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from camembert import predict_ner_camembert
-from bluebert import predict_ner_bluebert
-from bioclinicalbert import predict_ner_bioclinicalbert
+from english_ner import predict_english_ner
 
 from pydantic import BaseModel
-
 
 class Data(BaseModel):
     text: str
@@ -38,13 +36,41 @@ async def predict_entities(data: Data):
     return predictions
 
 
-@app.post("/ner/bluebert")
+@app.post("/ner/ncbi/bluebert")
 async def predict_entities(data: Data):
-    predictions = await predict_ner_bluebert(data.text)
+    id2label = {0: 'O', 1: 'B-disease', 2: 'I-disease'}
+    model_id = "data/clinical_ner_bluebert_pubmed_mimic_uncased"
+    predictions = await predict_english_ner(data.text, model_id, id2label)
     return predictions
 
 
-@app.post("/ner/clinicalbert")
+@app.post("/ner/ncbi/clinicalbert")
 async def predict_entities(data: Data):
-    predictions = await predict_ner_bioclinicalbert(data.text)
+    id2label = {0: 'O', 1: 'B-disease', 2: 'I-disease'}
+    model_id = "data/clinical_ner_Bio_ClinicalBert"
+    predictions = await predict_english_ner(data.text, model_id, id2label)
+    return predictions
+
+
+@app.post("/ner/bc5cdr/bluebert")
+async def predict_entities(data: Data):
+    id2label = {0: 'I-Disease', 1: 'B-Chemical', 2: 'I-Chemical', 3: 'O', 4: 'B-Disease'}
+    model_id = "data/BC5CDR_bluebert_pubmed_mimic_uncased"
+    predictions = await predict_english_ner(data.text, model_id, id2label)
+    return predictions
+
+
+@app.post("/ner/bionlp13cg/bluebert")
+async def predict_entities(data: Data):
+    id2label = {0: 'I-Immaterial_anatomical_entity', 1: 'B-Developing_anatomical_structure', 2: 'B-Pathological_formation',
+                3: 'O', 4: 'B-Cancer', 5: 'I-Gene_or_gene_product', 6: 'I-Anatomical_system', 7: 'B-Organism_substance',
+                8: 'I-Amino_acid', 9: 'I-Developing_anatomical_structure', 10: 'I-Tissue', 11: 'I-Organism',
+                12: 'B-Organism_subdivision', 13: 'I-Organism_substance', 14: 'I-Cellular_component',
+                15: 'I-Organism_subdivision', 16: 'B-Simple_chemical', 17: 'B-Immaterial_anatomical_entity',
+                18: 'B-Cellular_component', 19: 'B-Tissue', 20: 'B-Cell', 21: 'B-Amino_acid', 22: 'I-Cancer',
+                23: 'I-Simple_chemical', 24: 'I-Multi-tissue_structure', 25: 'I-Cell', 26: 'B-Gene_or_gene_product',
+                27: 'B-Organism', 28: 'I-Pathological_formation', 29: 'I-Organ', 30: 'B-Organ',
+                31: 'B-Multi-tissue_structure', 32: 'B-Anatomical_system'}
+    model_id = "data/BioNLP13CG_bluebert_5epochs"
+    predictions = await predict_english_ner(data.text, model_id, id2label)
     return predictions
